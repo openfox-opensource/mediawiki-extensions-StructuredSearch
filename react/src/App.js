5a8b6667f481bed31e4f732d4827c9ead07f4e3a
Itsick krumbein <any.site.n.site@gmail.com>
2019-06-27 22:57:17 +0300

    hide is default

--- react/src/App.js
+++ react/src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = { data: [],hide:true };
     EventEmitter.on("toggleSidebar", allData => {
       this.toggle( );
     });
