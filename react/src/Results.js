533a1dc7de1e2d2bd7341888f0ef5679a25f14fd
Chaya Kravitz <ckravitz@google.com>
2022-05-03 13:55:31 +0300

    link to search word

--- react/src/Results.js
+++ react/src/Results.js
@@ -12,37 +12,38 @@ class Results extends Component {
   constructor() {
     let params = queryString.parse(window.location.search);
     super();
+
     this.state = {
-      searchStarted : params.advanced_search ? true : false
+      searchStarted: params.advanced_search ? true : false
     };
 
-    translate('structuredsearch-no-results').then( translatedStr =>{
-        this.noResults = translatedStr;
+    translate('structuredsearch-no-results').then(translatedStr => {
+      this.noResults = translatedStr;
     });
-    translate('structuredsearch-on-results-error').then( translatedStr =>{
-        this.noResultsError = translatedStr;
-    });    
-    translate('structuredsearch-next').then( translatedStr =>{
-        this.nextText = translatedStr;
+    translate('structuredsearch-on-results-error').then(translatedStr => {
+      this.noResultsError = translatedStr;
     });
-    translate('structuredsearch-on-search-text').then( translatedStr =>{
-        this.onSearchText = translatedStr;
+    translate('structuredsearch-next').then(translatedStr => {
+      this.nextText = translatedStr;
     });
-    translate('structuredsearch-results-sum').then( translatedStr =>{
-        this.resultsSumText = translatedStr;
+    translate('structuredsearch-on-search-text').then(translatedStr => {
+      this.onSearchText = translatedStr;
+    });
+    translate('structuredsearch-results-sum').then(translatedStr => {
+      this.resultsSumText = translatedStr;
     });
     EventEmitter.on('searchStarted', data => {
       let paramsToChange = {
-        searchStarted:true,
-        onTop:data.reset,
+        searchStarted: true,
+        onTop: data.reset,
       };
-      if(data.reset){
+      if (data.reset) {
         paramsToChange.results = [];
         paramsToChange.lastIsError = false;
         paramsToChange.total = 0;
-        setTimeout(()=>{
+        setTimeout(() => {
           this.scrollUp();
-        },200);
+        }, 200);
       }
       this.setState(paramsToChange);
 
@@ -50,110 +51,158 @@ class Results extends Component {
     EventEmitter.on('dataRecieved', data => {
       let results = data.results;
       //console.log("results",results,data);
-      if(results && results.error){
+      if (results && results.error) {
         this.setState({
-          lastIsError:true,
-          results:[],
-          searchReturned:true,
-          searchStarted:false,
-          onTop:false
+          lastIsError: true,
+          results: [],
+          searchReturned: true,
+          searchStarted: false,
+          onTop: false
         })
       }
-      else{
+      else {
         let newResults;
-        if(!data.reset){
+        if (!data.reset) {
           newResults = this.state.results || {};
           newResults = Object.assign(newResults, results);
         }
-        else{
+        else {
           newResults = results;
         }
-        let newTotal = (data.searchinfo?.totalhits || 0) + ( data.reset ? 0 : this.state.total );
+        let newTotal = (data.searchinfo?.totalhits || 0) + (data.reset ? 0 : this.state.total);
         this.setState({
           offset: data.continue ? data.continue.sroffset : null,
           total: newTotal,
-          lastIsError:false,
-          results:newResults,
-          searchReturned:true,
-          searchStarted:false,
-          onTop:false
+          lastIsError: false,
+          results: newResults,
+          searchReturned: true,
+          searchStarted: false,
+          onTop: false
         })
       }
 
     });
     settingsGetter.get().then(data => {
-        //console.log(data.templates, data);
-        if( data ){
-          this.templates = data.templates;
-        }
+      //console.log(data.templates, data);
+      if (data) {
+        this.templates = data.templates;
+      }
     });
   }
-  resultClicked( title, event ){
-    console.log("resultClicked StructuredSearchResultclicked", title, event);
-    FormMain.fireGlobalEvent( {title:title}, 'StructuredSearchResultclicked');
+  resultClicked(title, event) {
+    console.log("rererr");
+    // console.log("resultClicked StructuredSearchResultclicked", title, event);
+    FormMain.fireGlobalEvent({ title: title }, 'StructuredSearchResultclicked');
   }
-  getTempalteByResult( result ){
+  getTempalteByResult(result) {
     let ns = result.namespaceId;
     return this.templates['template_' + ns] || this.templates['default'];
   }
-  getResultJsx( result ){
-    let template = this.getTempalteByResult( result ); 
-    //console.log(template,'template',result);
-    return <ReactMustache template={template} data={result} onClick={this.resultClicked.bind(result.full_title, this)}/>;
+  spllit(words) {
+    let wordArray = words.split(' ');
+    console.log(wordArray);
+    return wordArray;
   }
-  scrollUp(){
+  searchStringInArray(str, strArray) {
+    for (var j = 0; j < strArray.length; j++) {
+      if (strArray[j].match(str)) return j;
+    }
+    return -1;
+
+  }
+  insertWordToLink(index, arrayStringOfSearch,wordOfSearch) {//index to word search in array
+    let arrayWordToLink = new Array;
+    
+    for (var i = index - 3, j = 0; i < index + 4; i++, j++) {
+      if(index==i)
+        arrayWordToLink[j]=wordOfSearch;
+      else if ( index - 1 != i)
+        arrayWordToLink[j] = arrayStringOfSearch[i];
+      else j--;
+
+    }
+    return arrayWordToLink;
+  }
+  linkToWordSearch(result) {
+    let indexOfSnippet = result.snippet.indexOf("a");
+    result.snippet = result.snippet.replaceAll('span', 'a');//Creat link
+    let arrayStringSnippet = this.spllit(result.snippet);
+    let title = result.full_title;
+    let wordSearch = document.querySelector('.field-wrp-name-search');
+    let wordInput = wordSearch.getElementsByTagName('input');// Find the search text
+    let wordOfSearch;
+    if (wordInput.length) {
+      wordOfSearch = wordInput[0].value;
+    }
+    let stringToSearch = '>' + wordOfSearch + '<';
+    let locationWordSearch = this.searchStringInArray(stringToSearch, arrayStringSnippet)
+    let arrayToLink = this.insertWordToLink(locationWordSearch, arrayStringSnippet,wordOfSearch);
+    let protocol = window.location.protocol;
+    if(wordOfSearch)
+      result.snippet = result.snippet.substring(0, indexOfSnippet) + ' href="' + protocol + '/' + title + '#:~:text=' + arrayToLink[0] + '%20' + arrayToLink[1] + '-,' + arrayToLink[2] + '%20' + arrayToLink[3] + ',' + arrayToLink[4] + '%20' + arrayToLink[5] + '" ' + result.snippet.substring(indexOfSnippet, result.snippet.length);
+  }
+
+  getResultJsx(result) {
+    let template = this.getTempalteByResult(result);
+    this.linkToWordSearch(result);
+    return <ReactMustache template={template} data={result} onClick={this.resultClicked.bind(result.full_title, this)} />;
+  }
+  scrollUp() {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth'
     });
   }
-  next(){
-    FormMain.setNext( this.state.offset );
+  next() {
+    FormMain.setNext(this.state.offset);
   }
-  render(){
-  	let results =[], errorHtml,nextButton, 
-        resultsSum='',searchTop ='', searchBottom = '';
-    if(this.state.searchStarted){
-      if(this.state.onTop){
-        searchTop = <div className="loading loading-top"  dangerouslySetInnerHTML={{__html:this.onSearchText}}></div>
+  render() {
+    let results = [], errorHtml, nextButton,
+      resultsSum = '', searchTop = '', searchBottom = '';
+    if (this.state.searchStarted) {
+      if (this.state.onTop) {
+        searchTop = <div className="loading loading-top" dangerouslySetInnerHTML={{ __html: this.onSearchText }}></div>
       }
-      else{
-        searchBottom = <div className="loading loading-bottom" dangerouslySetInnerHTML={{__html:this.onSearchText}}></div>
+      else {
+        searchBottom = <div className="loading loading-bottom" dangerouslySetInnerHTML={{ __html: this.onSearchText }}></div>
       }
     }
-    if(this.state.results){
-      for(let resultKey of Object.keys(this.state.results)){
+    if (this.state.results) {
+      for (let resultKey of Object.keys(this.state.results)) {
         let result = this.state.results[resultKey];
-        results.push(this.getResultJsx( result ) )
+        results.push(this.getResultJsx(result))
       }
-      if( this.state.searchReturned && !Object.keys(this.state.results).length){
-        if( this.state.lastIsError ){
-          errorHtml = <div className="no-results no-results-error" key={'error'}  dangerouslySetInnerHTML={{__html:this.noResultsError}}></div>
+      if (this.state.searchReturned && !Object.keys(this.state.results).length) {
+        if (this.state.lastIsError) {
+          errorHtml = <div className="no-results no-results-error" key={'error'} dangerouslySetInnerHTML={{ __html: this.noResultsError }}></div>
         }
-        else{
-          errorHtml = <div className="no-results no-results-empty" key={'error'} dangerouslySetInnerHTML={{__html:this.noResults}}></div>;
+        else {
+          errorHtml = <div className="no-results no-results-empty" key={'error'} dangerouslySetInnerHTML={{ __html: this.noResults }}></div>;
         }
-        
+
       }
-      //console.log("this.state.offset && results.length",this.state.offset , results.length)
-      if(this.state.offset && results.length){
-        nextButton = <button type="button" onClick={this.next.bind(this)} dangerouslySetInnerHTML={{__html:this.nextText}}></button>
+      // console.log("this.state.offset && results.length",this.state.offset , results.length)÷÷
+      if (this.state.offset && results.length) {
+
+        nextButton = <button type="button" onClick={this.next.bind(this)} dangerouslySetInnerHTML={{ __html: this.nextText }}></button>
       }
     }
-    if(results && this.resultsSumText && this.state.total){
+    if (results && this.resultsSumText && this.state.total) {
+
+
       let resultText = this.resultsSumText.replace('$1', this.state.total)
-      resultsSum = <div className="results-sum-message"  dangerouslySetInnerHTML={{__html:resultText}}></div>;
+      resultsSum = <div className="results-sum-message" dangerouslySetInnerHTML={{ __html: resultText }}></div>;
     }
     return <div className='results'>
-            	{searchTop}
-              {resultsSum}
-              {results}
-              {errorHtml}
-              {nextButton}
-              {searchBottom}
-     		</div>;
+      {searchTop}
+      {resultsSum}
+      {results}
+      {errorHtml}
+      {nextButton}
+      {searchBottom}
+    </div>;
   }
- }
+}
 
- export default Results;
+export default Results;
