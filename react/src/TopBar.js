ad65081e1978f64dc0ed05ceecfa0325d6b4c3af
Yitzhak Karov (Krumbein) <itsik@openfox.io>
2022-07-04 16:24:26 +0300

    remove console.log

--- react/src/TopBar.js
+++ react/src/TopBar.js
@@ -83,7 +83,6 @@ class TopBar extends Component {
     return item;
   }
   removeLabel( fieldName, valueObj) {
-    console.log("fieldName, valueObj",this.state.inputs[fieldName], this.state.inputs,fieldName, valueObj);
     if('undefined' !== typeof this.state.inputs && ['range', 'dateRange'].includes(this.state.inputs[fieldName].widget.type) ){
       FormMain.clearField(fieldName);
     }
