fabd3a8358fd7f8e82c38ad376b8cb4651ab0cdb
Yitzhak Karov (Krumbein) <itsik@openfox.io>
2025-12-08 19:28:18 +0200

    fix for mobile, lets see desktop

--- react/src/TopBar.js
+++ react/src/TopBar.js
@@ -144,6 +144,14 @@ class TopBar extends Component {
   };
   
   setStickyCheck( ) {
+    // Don't enable sticky on mobile parser-search view
+    if (utils.isMobile() && typeof document !== 'undefined') {
+      const topBarContainer = document.getElementById('top-bar');
+      if (topBarContainer && topBarContainer.closest('.parser-search')) {
+        return; // Skip sticky observer for mobile parser-search
+      }
+    }
+    
     const observer = new IntersectionObserver((records, observer) => {
       for (const record of records) {
         const targetInfo = record.boundingClientRect;
@@ -155,7 +163,10 @@ class TopBar extends Component {
         }
       }
     }, {threshold: [0,1]});
-    observer.observe(document.querySelector('.checking-sticky'))
+    const checkingElement = document.querySelector('.checking-sticky');
+    if (checkingElement) {
+      observer.observe(checkingElement);
+    }
   }
   standardizeItem( item) {
     if( 'string' === typeof item ){
@@ -351,7 +362,34 @@ class TopBar extends Component {
       <div className={'TopBar sticky-top' + appendedClass}>
         <header className="App-header">
           <form onSubmit={this.submitClicked.bind(this)}>
-            {searchInput}
+            <div className="search-input-with-toggle">
+              {searchInput}
+              {this.state.enableDisplayToggle && (
+                <div className="display-buttons inline-flex flex-row-reverse gap-2">
+                  <button
+                    type="button"
+                    id="grid-button"
+                    className="flex align-items-center justify-center"
+                    onClick={() => EventEmitter.emit('toggleDisplayView', false)}
+                  >
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd" d="M3 3V11H11V3H3ZM9 9H5V5H9V9ZM3 13V21H11V13H3ZM9 19H5V15H9V19ZM13 3V11H21V3H13ZM19 9H15V5H19V9ZM13 13V21H21V13H13ZM19 19H15V15H19V19Z" fill="#5F6368"></path>
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    id="card-button"
+                    className="isDisplay flex align-items-center justify-center"
+                    onClick={() => EventEmitter.emit('toggleDisplayView', true)}
+                  >
+                      <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd" d="M0.5 2C0.5 2.82843 1.17157 3.5 2 3.5C2.82843 3.5 3.5 2.82843 3.5 2C3.5 1.17157 2.82843 0.5 2 0.5C1.17157 0.5 0.5 1.17157 0.5 2ZM7 1C6.44772 1 6 1.44772 6 2C6 2.55228 6.44771 3 7 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H7ZM7 7C6.44772 7 6 7.44772 6 8C6 8.55228 6.44771 9 7 9H16C16.5523 9 17 8.55228 17 8C17 7.44772 16.5523 7 16 7H7ZM6 14C6 13.4477 6.44772 13 7 13H16C16.5523 13 17 13.4477 17 14C17 14.5523 16.5523 15 16 15H7C6.44771 15 6 14.5523 6 14ZM2 9.5C1.17157 9.5 0.5 8.82843 0.5 8C0.5 7.17157 1.17157 6.5 2 6.5C2.82843 6.5 3.5 7.17157 3.5 8C3.5 8.82843 2.82843 9.5 2 9.5ZM0.5 14C0.5 14.8284 1.17157 15.5 2 15.5C2.82843 15.5 3.5 14.8284 3.5 14C3.5 13.1716 2.82843 12.5 2 12.5C1.17157 12.5 0.5 13.1716 0.5 14Z" fill="black"></path>
+                    </svg>
+                    
+                  </button>
+                </div>
+              )}
+            </div>
             {otherTopbarInputs.length > 0 && (
               <div className="topbar-other-fields">
                 {otherTopbarInputs}
@@ -364,31 +402,6 @@ class TopBar extends Component {
           {!isFilterHidden && (
           <button type="button" onClick={this.clearClicked.bind( this )}  dangerouslySetInnerHTML={{__html:this.state['structuredsearch-clear']}} ></button>
         )}
-        {this.state.enableDisplayToggle && (
-  <div className="display-buttons inline-flex flex-row-reverse gap-2">
-    <button
-      type="button"
-      id="grid-button"
-      className="flex align-items-center justify-center"
-      onClick={() => EventEmitter.emit('toggleDisplayView', false)}
-    >
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" clipRule="evenodd" d="M3 3V11H11V3H3ZM9 9H5V5H9V9ZM3 13V21H11V13H3ZM9 19H5V15H9V19ZM13 3V11H21V3H13ZM19 9H15V5H19V9ZM13 13V21H21V13H13ZM19 19H15V15H19V19Z" fill="#5F6368"></path>
-      </svg>
-    </button>
-    <button
-      type="button"
-      id="card-button"
-      className="isDisplay flex align-items-center justify-center"
-      onClick={() => EventEmitter.emit('toggleDisplayView', true)}
-    >
-        <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" clipRule="evenodd" d="M0.5 2C0.5 2.82843 1.17157 3.5 2 3.5C2.82843 3.5 3.5 2.82843 3.5 2C3.5 1.17157 2.82843 0.5 2 0.5C1.17157 0.5 0.5 1.17157 0.5 2ZM7 1C6.44772 1 6 1.44772 6 2C6 2.55228 6.44771 3 7 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H7ZM7 7C6.44772 7 6 7.44772 6 8C6 8.55228 6.44771 9 7 9H16C16.5523 9 17 8.55228 17 8C17 7.44772 16.5523 7 16 7H7ZM6 14C6 13.4477 6.44772 13 7 13H16C16.5523 13 17 13.4477 17 14C17 14.5523 16.5523 15 16 15H7C6.44771 15 6 14.5523 6 14ZM2 9.5C1.17157 9.5 0.5 8.82843 0.5 8C0.5 7.17157 1.17157 6.5 2 6.5C2.82843 6.5 3.5 7.17157 3.5 8C3.5 8.82843 2.82843 9.5 2 9.5ZM0.5 14C0.5 14.8284 1.17157 15.5 2 15.5C2.82843 15.5 3.5 14.8284 3.5 14C3.5 13.1716 2.82843 12.5 2 12.5C1.17157 12.5 0.5 13.1716 0.5 14Z" fill="black"></path>
-      </svg>
-      
-    </button>
-  </div>
-)}
           </header>
 
     </div> : <div className='TopBar TopBar-loader'></div>;
